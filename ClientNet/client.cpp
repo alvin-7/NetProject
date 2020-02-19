e1da8a2086f77bc5f5d372b4b736a6e29108321f
zhuye-7 <38536867+zhuye-7@users.noreply.github.com>
2020-02-19 23:15:23 +0800

    Linux平台

--- ClientNet/client.cpp
+++ ClientNet/client.cpp
@@ -1,8 +1,19 @@
 #define WIN32_LEAN_AND_MEAN
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 
-#include <windows.h>
-#include <WinSock2.h>
+#ifdef _WIN32
+	#include <windows.h>
+	#include <WinSock2.h>
+#else
+	#include <unistd.h>
+	#include <arpa/inet.h>
+	#include <string.h>
+	
+	#define SOCKET int
+	#define INVALID_SOCKET  (SOCKET)(~0)
+	#define SOCKET_ERROR            (-1)
+#endif // _WIN32
+
 #include <stdio.h>
 #include <thread>
 #include "defines.h"
